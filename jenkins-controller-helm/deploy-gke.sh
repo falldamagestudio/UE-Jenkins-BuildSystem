a3627dee7ed84c956e377e1213d634c83e75f86f
Mikael Kalms <mikael@kalms.org>
2020-12-17 21:01:57 +0100

    Use  for smart install-or-upgrade

--- jenkins-controller-helm/deploy-gke.sh
+++ jenkins-controller-helm/deploy-gke.sh
@@ -16,10 +16,4 @@ REGION="europe-west1"
 
 IMAGE="${REGION}-docker.pkg.dev/${PROJECT}/docker-build-artifacts/ue-jenkins-controller"
 
-if [[ `helm list --filter ${RELEASE} -o json | jq ".[0]"` != "null" ]]; then
-    OPERATION="upgrade"
-else
-    OPERATION="install"
-fi
-
-helm ${OPERATION} --values ${VALUES} --values values/gke/serviceType.yaml --values values/gke/nodeSelector.yaml --values values/gke/ingress.yaml --values values/gke/gke-image.yaml --version=${VERSION} ${RELEASE} ${CHART} --debug --set GOOGLE_OAUTH_CLIENT_ID=${GOOGLE_OAUTH_CLIENT_ID} --set GOOGLE_OAUTH_CLIENT_SECRET=${GOOGLE_OAUTH_CLIENT_SECRET} --set controller.ingress.annotations.kubernetes\\.io/ingress\\.global-static-ip-name=${STATIC_IP_ADDRESS_NAME} --set controller.image=${IMAGE} --set controller.tag=${IMAGE_TAG} --set controller.ingress.hostName=$5
+helm upgrade --install --values ${VALUES} --values values/gke/serviceType.yaml --values values/gke/nodeSelector.yaml --values values/gke/ingress.yaml --values values/gke/gke-image.yaml --version=${VERSION} ${RELEASE} ${CHART} --debug --set GOOGLE_OAUTH_CLIENT_ID=${GOOGLE_OAUTH_CLIENT_ID} --set GOOGLE_OAUTH_CLIENT_SECRET=${GOOGLE_OAUTH_CLIENT_SECRET} --set controller.ingress.annotations.kubernetes\\.io/ingress\\.global-static-ip-name=${STATIC_IP_ADDRESS_NAME} --set controller.image=${IMAGE} --set controller.tag=${IMAGE_TAG} --set controller.ingress.hostName=$5
--- jenkins-controller-helm/deploy-local.sh
+++ jenkins-controller-helm/deploy-local.sh
@@ -8,10 +8,4 @@ CHART="jenkins/jenkins"
 VERSION="v3.0.2"
 VALUES="values/values.yaml"
 
-if [[ `helm list --filter ${RELEASE} -o json | jq ".[0]"` != "null" ]]; then
-    OPERATION="upgrade"
-else
-    OPERATION="install"
-fi
-
-helm ${OPERATION} --values ${VALUES} --values values/local/jenkinsUrl.yaml --values values/local/local-image.yaml --version=${VERSION} ${RELEASE} ${CHART} --debug --set GOOGLE_OAUTH_CLIENT_ID=${GOOGLE_OAUTH_CLIENT_ID} --set GOOGLE_OAUTH_CLIENT_SECRET=${GOOGLE_OAUTH_CLIENT_SECRET}
+helm upgrade --install --values ${VALUES} --values values/local/jenkinsUrl.yaml --values values/local/local-image.yaml --version=${VERSION} ${RELEASE} ${CHART} --debug --set GOOGLE_OAUTH_CLIENT_ID=${GOOGLE_OAUTH_CLIENT_ID} --set GOOGLE_OAUTH_CLIENT_SECRET=${GOOGLE_OAUTH_CLIENT_SECRET}
