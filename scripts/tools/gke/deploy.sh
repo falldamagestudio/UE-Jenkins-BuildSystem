c1b1a87a3cd2626c1fb43dd3d8928e280dc75a3b
Mikael Kalms <mikael@kalms.org>
2021-03-13 17:44:27 +0100

    Parameterized logic that picks up values files

--- scripts/tools/gke/deploy.sh
+++ scripts/tools/gke/deploy.sh
@@ -20,21 +20,43 @@ CONTROLLER_IMAGE_TAG=${CONTROLLER_IMAGE_AND_TAG##*:}
 UE_JENKINS_BUILDTOOLS_LINUX_IMAGE=`echo $HELM_CONFIG_JSON | jq -r ".ue_jenkins_buildtools_linux_image"`
 UE_JENKINS_BUILDTOOLS_WINDOWS_IMAGE=`echo $HELM_CONFIG_JSON | jq -r ".ue_jenkins_buildtools_windows_image"`
 
+PLASTIC=`echo $HELM_CONFIG_JSON | jq -r ".plastic"`
+
 RELEASE="jenkins-controller"
 CHART_NAME=`echo $HELM_CONFIG_JSON | jq -r ".chart_name"`
 CHART_VERSION=`echo $HELM_CONFIG_JSON | jq -r ".chart_version"`
 
 SEED_JOB_URL=`echo $HELM_CONFIG_JSON | jq -r ".seed_job_url"`
 
+VALUES_FILES=("${APPLICATION_DIR}/values/values.yaml" )
+
+# Include GKE-related values files
+readarray -d '' VALUES_FILES_GKE < <(find "${APPLICATION_DIR}/values/gke" -name '*.yaml' -print0)
+VALUES_FILES+=(${VALUES_FILES_GKE[@]})
+
+# Include Plastic-related values files, if Plastic is enabled in config
+if [[ ${PLASTIC} -eq "true" ]]; then
+
+    readarray -d '' VALUES_FILES_PLASTIC < <(find "${APPLICATION_DIR}/values/plastic" -name '*.yaml' -print0)
+    VALUES_FILES+=(${VALUES_FILES_PLASTIC[@]})
+
+fi
+
+# Convert an array like this:
+#  a.yaml b.yaml c.yaml
+# to this:
+#  --values a.yaml --values b.yaml --values c.yaml
+
+VALUES=()
+
+for VALUES_FILE in "${VALUES_FILES[@]}"
+do
+    VALUES+=("--values" "${VALUES_FILE}")
+done
+
 helm upgrade \
     --install \
-    --values "${APPLICATION_DIR}/values/values.yaml" \
-    --values "${APPLICATION_DIR}/values/gke/serviceType.yaml" \
-    --values "${APPLICATION_DIR}/values/gke/nodeSelector.yaml" \
-    --values "${APPLICATION_DIR}/values/gke/ingress.yaml" \
-    --values "${APPLICATION_DIR}/values/gke/gke-image.yaml" \
-    --values "${APPLICATION_DIR}/values/gke/serviceAnnotations.yaml" \
-    --values "${APPLICATION_DIR}/values/plastic/volumes.yaml" \
+    ${VALUES[*]} \
     --version=${CHART_VERSION} \
     ${RELEASE} \
     ${CHART_NAME} \
--- scripts/tools/local/deploy.sh
+++ scripts/tools/local/deploy.sh
@@ -16,18 +16,43 @@ CONTROLLER_IMAGE_TAG=${CONTROLLER_IMAGE_AND_TAG##*:}
 
 UE_JENKINS_BUILDTOOLS_LINUX_IMAGE=`echo $HELM_CONFIG_JSON | jq -r ".ue_jenkins_buildtools_linux_image"`
 
+PLASTIC=`echo $HELM_CONFIG_JSON | jq -r ".plastic"`
+
 RELEASE="jenkins-controller"
 CHART_NAME=`echo $HELM_CONFIG_JSON | jq -r ".chart_name"`
 CHART_VERSION=`echo $HELM_CONFIG_JSON | jq -r ".chart_version"`
 
 SEED_JOB_URL=`echo $HELM_CONFIG_JSON | jq -r ".seed_job_url"`
 
+VALUES_FILES=("${APPLICATION_DIR}/values/values.yaml" )
+
+# Include local-related values files
+readarray -d '' VALUES_FILES_LOCAL < <(find "${APPLICATION_DIR}/values/local" -name '*.yaml' -print0)
+VALUES_FILES+=(${VALUES_FILES_LOCAL[@]})
+
+# Include Plastic-related values files, if Plastic is enabled in config
+if [[ ${PLASTIC} -eq "true" ]]; then
+
+    readarray -d '' VALUES_FILES_PLASTIC < <(find "${APPLICATION_DIR}/values/plastic" -name '*.yaml' -print0)
+    VALUES_FILES+=(${VALUES_FILES_PLASTIC[@]})
+
+fi
+
+# Convert an array like this:
+#  a.yaml b.yaml c.yaml
+# to this:
+#  --values a.yaml --values b.yaml --values c.yaml
+
+VALUES=()
+
+for VALUES_FILE in "${VALUES_FILES[@]}"
+do
+    VALUES+=("--values" "${VALUES_FILE}")
+done
+
 helm upgrade \
     --install \
-    --values "${APPLICATION_DIR}/values/values.yaml" \
-    --values "${APPLICATION_DIR}/values/local/jenkinsUrl.yaml" \
-    --values "${APPLICATION_DIR}/values/local/local-image.yaml" \
-    --values "${APPLICATION_DIR}/values/plastic/volumes.yaml" \
+    ${VALUES[*]} \
     --version=${CHART_VERSION} ${RELEASE} ${CHART_NAME} \
     --debug \
     --set controller.image=${CONTROLLER_IMAGE} \
