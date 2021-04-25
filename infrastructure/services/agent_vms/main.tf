978b8d6a47ee12199791c6873f3959cafd381b62
Mikael Kalms <mikael@kalms.org>
2021-04-25 14:22:31 +0200

    Allow agent VMs to write to Cloud Logging

--- infrastructure/services/agent_vms/main.tf
+++ infrastructure/services/agent_vms/main.tf
@@ -4,6 +4,7 @@ locals {
   ) + length(google_compute_firewall.agent_vms_allow_winrm.id
   ) + length(google_project_iam_member.agent_build_artifact_downloader_access.id
   ) + length(google_storage_bucket_iam_member.agent_longtail_store_admin_access.id
+  ) + length(google_project_iam_member.agent_cloud_logging_write_access.id
   ) + length(google_compute_network_peering.kubernetes_network_agent_vms_peering.id
   ) + length(google_compute_network_peering.agent_vms_kubernetes_network_peering.id
   ) + length(google_compute_instance.engine_win64_agent.id)
@@ -65,3 +66,12 @@ resource "google_storage_bucket_iam_member" "agent_longtail_store_admin_access" 
   role     = "roles/storage.admin"
   member = "serviceAccount:${google_service_account.agent_service_account.email}"
 }
+
+# Allow agent VMs to send log messages to Cloud Logging
+resource "google_project_iam_member" "agent_cloud_logging_write_access" {
+  depends_on = [ var.module_depends_on ]
+
+  role   = "roles/logging.logWriter"
+  member = "serviceAccount:${google_service_account.agent_service_account.email}"
+}
+
