e4d3c5c9426c9e2fa640e66e82e1203c35ca6f6a
Mikael Kalms <mikael@kalms.org>
2021-05-17 12:08:22 +0200

    Create windows agent vms via Terraform

--- infrastructure/services/agent_vms/builder_vms.tf
+++ infrastructure/services/agent_vms/builder_vms.tf
@@ -1,10 +1,19 @@
 
 locals {
 
+    windows_build_agents = {
+/*
+        "build-game-windows-git-docker2" = {
+            "machine_type" = "n1-standard-8"
+            "boot_disk_size" = 50
+            "persistent_disk_size" = 200
+        }
+*/
+    }
+
     linux_build_agents = {
 /*
         "build-game-linux-git-docker2" = {
-            "name" = "build-game-linux-git-docker"
             "machine_type" = "n1-standard-8"
             "boot_disk_size" = 50
             "persistent_disk_size" = 200
@@ -19,7 +28,7 @@ resource "google_compute_instance" "linux_build_agent" {
 
     for_each = local.linux_build_agents
 
-    name = each.value.name
+    name = each.key
 
     machine_type = each.value.machine_type
 
@@ -63,7 +72,43 @@ resource "google_compute_instance" "linux_build_agent" {
 resource "google_compute_disk" "linux_build_agent_pd" {
  
     for_each = local.linux_build_agents
-    name  = "${each.value.name}-pd"
+    name  = "${each.key}-pd"
     size =  each.value.persistent_disk_size
     type  = "pd-ssd"
+}
+
+resource "google_compute_instance" "windows_build_agent" {
+
+    for_each = local.windows_build_agents
+
+    name = each.key
+
+    machine_type = each.value.machine_type
+
+    boot_disk {
+
+        initialize_params {
+            size = each.value.boot_disk_size
+            type = "pd-ssd"
+            image = "projects/kalms-ue4-jenkins-buildsystem/global/images/ue-jenkins-swarm-agent-vm-00b4ffb-windows" // TODO: Move to config
+        }
+
+    }
+
+    network_interface {
+
+        network = google_compute_network.agent_vms.name
+        subnetwork = google_compute_subnetwork.agent_vms.name
+
+        access_config {
+
+            // Auto-generate external IP
+
+        }
+    }
+
+    service_account {
+        email = google_service_account.agent_service_account.email
+        scopes = [ "cloud-platform" ]
+    }
 }
