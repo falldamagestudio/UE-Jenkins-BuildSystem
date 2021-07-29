fc33cc98037b6bda90cf413ffe53ae5ce5864dc9
Mikael Kalms <mikael@kalms.org>
2021-07-29 14:15:26 +0200

    lookup instance template via id

--- infrastructure/agents/static_agents/builder_vms.tf
+++ infrastructure/agents/static_agents/builder_vms.tf
@@ -4,7 +4,7 @@ resource "google_compute_instance_from_template" "linux_static_build_agent" {
 
     name = each.key
 
-    source_instance_template = each.value.template
+    source_instance_template = google_compute_instance_template.linux_agent_template[each.value.template].id
 }
 
 resource "google_compute_instance_from_template" "windows_static_build_agent" {
@@ -13,5 +13,5 @@ resource "google_compute_instance_from_template" "windows_static_build_agent" {
 
     name = each.key
 
-    source_instance_template = each.value.template
+    source_instance_template = google_compute_instance_template.windows_agent_template[each.value.template].id
 }
