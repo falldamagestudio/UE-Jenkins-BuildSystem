21025de0a9b084413585b869e98eb04bcc5d1c0a
Mikael Kalms <mikael@kalms.org>
2021-09-17 14:15:25 +0200

    allow RDP traffic to Windows Agent VMs

--- infrastructure/core/network/agent_vms_firewall.tf
+++ infrastructure/core/network/agent_vms_firewall.tf
@@ -31,3 +31,37 @@ resource "google_compute_firewall" "agent_vms_allow_ssh" {
 
   source_ranges = [ "0.0.0.0/0" ]
 }
+
+resource "google_compute_firewall" "agent_vms_allow_rdp_tcp" {
+  depends_on = [ var.module_depends_on ]
+
+  name = "agent-vms-allow-rdp-tcp"
+  
+  network = google_compute_network.agent_vms.name
+
+  description = "Allow Remote Desktop (TCP) to agent VMs"
+
+  allow {
+    protocol = "tcp"
+    ports = [ "3389" ]
+  }
+
+  source_ranges = [ "0.0.0.0/0" ]
+}
+
+resource "google_compute_firewall" "agent_vms_allow_rdp_udp" {
+  depends_on = [ var.module_depends_on ]
+
+  name = "agent-vms-allow-rdp-udp"
+  
+  network = google_compute_network.agent_vms.name
+
+  description = "Allow Remote Desktop (UDP) to agent VMs"
+
+  allow {
+    protocol = "udp"
+    ports = [ "3389" ]
+  }
+
+  source_ranges = [ "0.0.0.0/0" ]
+}
