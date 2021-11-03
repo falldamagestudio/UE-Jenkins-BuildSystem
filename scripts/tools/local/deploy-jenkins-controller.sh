39ac76099d7821b7a508825294062b3502b9a2f4
Mikael Kalms <mikael@kalms.org>
2021-11-03 22:46:50 +0100

    Use minimal custom Helm chart for Jenkins instance CR

--- scripts/tools/local/deploy-jenkins-controller.sh
+++ scripts/tools/local/deploy-jenkins-controller.sh
@@ -2,6 +2,8 @@
 
 APPLICATION_DIR="${BASH_SOURCE%/*}/../../../application"
 
+JENKINS_CHART_DIR="${APPLICATION_DIR}/chart"
+JENKINS_RELEASE="jenkins"
 JENKINS_NAMESPACE="jenkins"
 
 # Create namespace for jenkins instance
@@ -10,6 +12,13 @@ if ! kubectl get namespace "${JENKINS_NAMESPACE}" > /dev/null 2>&1; then
     kubectl create namespace "${JENKINS_NAMESPACE}"
 fi
 
-# Install/update/configure Jenkins CR
+# Install/update/configure Jenkins instance CR
 
-kubectl apply -f "${APPLICATION_DIR}/jenkins_instance.yaml"
+helm upgrade \
+    --install \
+    ${VALUES[*]} \
+    "${JENKINS_RELEASE}" \
+    "${JENKINS_CHART_DIR}" \
+    "--namespace" "${JENKINS_NAMESPACE}" \
+    --debug \
+    --wait
