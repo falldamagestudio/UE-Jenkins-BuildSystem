5358c15a51e7dec07dd2d28f20c72e6a4f9d8f75
Mikael Kalms <mikael@kalms.org>
2021-11-04 07:51:33 +0100

    Controller name + namespace is configurable

--- scripts/tools/local/deploy-jenkins-controller.sh
+++ scripts/tools/local/deploy-jenkins-controller.sh
@@ -6,6 +6,28 @@ JENKINS_CHART_DIR="${APPLICATION_DIR}/chart"
 JENKINS_RELEASE="jenkins"
 JENKINS_NAMESPACE="jenkins"
 
+VALUES_FILES=()
+
+# Include platform-independent values files
+readarray -d '' VALUES_FILES_DEFAULT < <(find "${APPLICATION_DIR}/values" -maxdepth 1 -name '*.yaml' -print0)
+VALUES_FILES+=(${VALUES_FILES_DEFAULT[@]})
+
+# Include local-related values files
+readarray -d '' VALUES_FILES_LOCAL < <(find "${APPLICATION_DIR}/values/local" -name '*.yaml' -print0)
+VALUES_FILES+=(${VALUES_FILES_LOCAL[@]})
+
+# Convert an array like this:
+#  a.yaml b.yaml c.yaml
+# to this:
+#  --values a.yaml --values b.yaml --values c.yaml
+
+VALUES=()
+
+for VALUES_FILE in "${VALUES_FILES[@]}"
+do
+    VALUES+=("--values" "${VALUES_FILE}")
+done
+
 # Create namespace for jenkins instance
 
 if ! kubectl get namespace "${JENKINS_NAMESPACE}" > /dev/null 2>&1; then
