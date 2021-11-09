cde561cb81fb4a8b343807967655af873697df2a
Mikael Kalms <mikael@kalms.org>
2021-11-09 14:22:00 +0100

    Created initial deploy-to-gke script

---
+++ scripts/tools/gke/deploy-jenkins-controller.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+HELM_CONFIG_FILE=$1
+
+APPLICATION_DIR="${BASH_SOURCE%/*}/../../../application"
+
+JENKINS_CHART_DIR="${APPLICATION_DIR}/chart"
+JENKINS_RELEASE="jenkins"
+
+HELM_CONFIG_JSON=$(cat "${HELM_CONFIG_FILE}")
+JENKINS_NAMESPACE=$(echo "${HELM_CONFIG_JSON}" | jq -r ".namespace")
+
+GOOGLE_OAUTH_CLIENT_ID=$(kubectl get secrets -n ${JENKINS_NAMESPACE} jenkins-controller-from-manual-config -o jsonpath="{.data.google_oauth_client_id}" | base64 --decode)
+GOOGLE_OAUTH_CLIENT_SECRET=$(kubectl get secrets -n ${JENKINS_NAMESPACE} jenkins-controller-from-manual-config -o jsonpath="{.data.google_oauth_client_secret}" | base64 --decode)
+
+VALUES_FILES=()
+
+# Include platform-independent values files
+readarray -d '' VALUES_FILES_DEFAULT < <(find "${APPLICATION_DIR}/values" -maxdepth 1 -name '*.yaml' -print0)
+VALUES_FILES+=(${VALUES_FILES_DEFAULT[@]})
+
+# Include gke-related values files
+readarray -d '' VALUES_FILES_GKE < <(find "${APPLICATION_DIR}/values/gke" -name '*.yaml' -print0)
+VALUES_FILES+=(${VALUES_FILES_GKE[@]})
+
+# Convert an array like this:
+#  a.yaml b.yaml c.yaml
+# to this:
+#  --values a.yaml --values b.yaml --values c.yaml
+
+VALUES=()
+
+for VALUES_FILE in "${VALUES_FILES[@]}"
+do
+    VALUES+=("--values" "${VALUES_FILE}")
+done
+
+# Create namespace for jenkins instance
+
+if ! kubectl get namespace "${JENKINS_NAMESPACE}" > /dev/null 2>&1; then
+    kubectl create namespace "${JENKINS_NAMESPACE}"
+fi
+
+# Install/update/configure Jenkins instance CR
+
+helm upgrade \
+    --install \
+    ${VALUES[*]} \
+    "${JENKINS_RELEASE}" \
+    "${JENKINS_CHART_DIR}" \
+    "--namespace" "${JENKINS_NAMESPACE}" \
+    --set "GOOGLE_OAUTH_CLIENT_ID=${GOOGLE_OAUTH_CLIENT_ID}" \
+    --set "GOOGLE_OAUTH_CLIENT_SECRET=${GOOGLE_OAUTH_CLIENT_SECRET}" \
+    --set "jenkins.namespace=${JENKINS_NAMESPACE}" \
+    --debug \
+    --wait
---
+++ scripts/tools/gke/deploy-jenkins-operator.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+HELM_CONFIG_FILE=$1
+
+OPERATOR_DIR=${BASH_SOURCE%/*}/../../../operator
+
+OPERATOR_CHART_NAME="jenkins-operator"
+OPERATOR_CHART_VERSION="0.5.3"
+OPERATOR_RELEASE="jenkins-operator"
+
+HELM_CONFIG_JSON=$(cat "${HELM_CONFIG_FILE}")
+OPERATOR_NAMESPACE=$(echo "${HELM_CONFIG_JSON}" | jq -r ".namespace")
+
+VALUES_FILES=()
+
+# Include platform-independent values files
+readarray -d '' VALUES_FILES_DEFAULT < <(find "${OPERATOR_DIR}/values" -maxdepth 1 -name '*.yaml' -print0)
+VALUES_FILES+=(${VALUES_FILES_DEFAULT[@]})
+
+# Include gke-related values files
+readarray -d '' VALUES_FILES_GKE < <(find "${OPERATOR_DIR}/values/gke" -name '*.yaml' -print0)
+VALUES_FILES+=(${VALUES_FILES_GKE[@]})
+
+# Convert an array like this:
+#  a.yaml b.yaml c.yaml
+# to this:
+#  --values a.yaml --values b.yaml --values c.yaml
+
+VALUES=()
+
+for VALUES_FILE in "${VALUES_FILES[@]}"
+do
+    VALUES+=("--values" "${VALUES_FILE}")
+done
+
+# Create namespace for jenkins operator
+
+if ! kubectl get namespace "${OPERATOR_NAMESPACE}" > /dev/null 2>&1; then
+    kubectl create namespace "${OPERATOR_NAMESPACE}"
+fi
+
+# Install/update/configure Jenkins Operator
+
+helm upgrade \
+    --install \
+    ${VALUES[*]} \
+    "${OPERATOR_RELEASE}" \
+    "${OPERATOR_CHART_NAME}" \
+    "--version" "${OPERATOR_CHART_VERSION}" \
+    "--repo" "https://raw.githubusercontent.com/jenkinsci/kubernetes-operator/master/chart" \
+    "--namespace" "${OPERATOR_NAMESPACE}" \
+    --debug \
+    --wait
