3e130defebb0f00bdbb9beb5d695501bc0a23297
Mikael Kalms <mikael@kalms.org>
2023-02-26 21:34:14 +0100

    Exposed controller VM SA key in Terraform output

--- config/fd/terraform/controller/values.tf
+++ config/fd/terraform/controller/values.tf
@@ -2,3 +2,8 @@ output "controller_vm_ansible_private_key" {
   value = module.controller.controller_vm_ansible_private_key
   sensitive = true
 }
+
+output "controller_vm_service_account_key" {
+  value = module.controller.controller_vm_service_account_key
+  sensitive = true
+}
--- infrastructure/controller/values.tf
+++ infrastructure/controller/values.tf
@@ -2,3 +2,8 @@ output "controller_vm_ansible_private_key" {
   value = tls_private_key.controller_vm_ansible_key.private_key_pem
   sensitive = true
 }
+
+output "controller_vm_service_account_key" {
+  value = base64decode(google_service_account_key.controller_service_account_key.private_key)
+  sensitive = true
+}
